9f5a32a92a9cbf87c305a80c09e47222d4c81449
Gupta <agupta3@bjs.com>
2021-03-12 08:14:36 -0500

    Changed logic for qr code generator and validator

--- src/main/java/com/example/QRCode/controller/QRCodeController.java
+++ src/main/java/com/example/QRCode/controller/QRCodeController.java
@@ -8,11 +8,11 @@ import org.json.simple.parser.ParseException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.example.QRCode.service.QRCodeService;
 import com.google.zxing.NotFoundException;
 import com.google.zxing.WriterException;
@@ -29,21 +29,26 @@ public class QRCodeController {
 	QRCodeService qrCodeService;
 
 	@RequestMapping(method = RequestMethod.POST, value = "/generate", produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<?> getMockDetails(@RequestBody String generationCode, HttpServletRequest request)
+	public ResponseEntity<?> getMockDetails()
 			throws IOException, ParseException, WriterException {
 		try {
-			return ResponseEntity.ok().body(qrCodeService.generateQRCode(generationCode));
+			return ResponseEntity.ok().body(qrCodeService.generateQRCode());
 		} catch (IOException e) {
 			return ResponseEntity.notFound().build();
 
 		}
 	}
 
-	@RequestMapping(method = RequestMethod.GET, value = "/validate", produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<?> postMockDetails(HttpServletRequest request)
+	@SuppressWarnings("deprecation")
+	@RequestMapping(method = RequestMethod.GET, value = "/validate/{generatedCode}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<?> postMockDetails(@PathVariable String generatedCode, HttpServletRequest request)
 			throws IOException, ParseException, NotFoundException, WriterException {
 		try {
-			return ResponseEntity.ok().body(qrCodeService.validateQRCode());
+			String res = qrCodeService.validateQRCode(generatedCode);
+			if (!StringUtils.isEmpty(res) && !res.equalsIgnoreCase("badRequest")) {
+				return ResponseEntity.ok().body(res);
+			}
+			return ResponseEntity.badRequest().body(res);
 		} catch (IOException e) {
 			return ResponseEntity.notFound().build();
 		}
--- src/main/java/com/example/QRCode/service/QRCodeService.java
+++ src/main/java/com/example/QRCode/service/QRCodeService.java
@@ -3,8 +3,11 @@ package com.example.QRCode.service;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.UUID;
+
 import javax.imageio.ImageIO;
 
+import org.omg.CORBA.BAD_PARAM;
 import org.springframework.stereotype.Service;
 
 import com.google.zxing.BarcodeFormat;
@@ -24,20 +27,26 @@ import com.google.zxing.Result;
 public class QRCodeService {
 
 	@SuppressWarnings("deprecation")
-	public String generateQRCode(String data) throws WriterException, IOException {
+	public String generateQRCode() throws WriterException, IOException {
+		String data= UUID.randomUUID().toString();
 		String path = System.getProperty("user.dir") + "//resources//QRCode.png";
 		BitMatrix matrix = new MultiFormatWriter().encode(new String(data.getBytes("UTF-8"), "UTF-8"),
 				BarcodeFormat.QR_CODE, 200, 200);
 		MatrixToImageWriter.writeToFile(matrix, path.substring(path.lastIndexOf('.') + 1), new File(path));
-		return "success";
+		return data;
 	}
 
-	public String validateQRCode() throws WriterException, IOException, NotFoundException {
+	public String validateQRCode(String data) throws WriterException, IOException, NotFoundException {
 		String path = System.getProperty("user.dir") + "//resources//QRCode.png";
 		BinaryBitmap binaryBitmap = new BinaryBitmap(
 				new HybridBinarizer(new BufferedImageLuminanceSource(ImageIO.read(new FileInputStream(path)))));
 		Result rslt = new MultiFormatReader().decode(binaryBitmap);
-		return rslt.getText();
+		if(rslt.getText().equalsIgnoreCase(data)) {
+			return rslt.getText();	
+		}
+		else {
+			return "badRequest";
+		}
 	}
 
 }
